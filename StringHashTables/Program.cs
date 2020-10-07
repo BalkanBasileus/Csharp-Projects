5eee454b37066d06e653e1fa507605a9e44e77f6
BalkanBasileus <54082287+BalkanBasileus@users.noreply.github.com>
2020-10-07 12:36:20 -0700

    Update Program.cs

--- StringHashTables/Program.cs
+++ StringHashTables/Program.cs
@@ -1,19 +1,29 @@
-﻿using System;
+/*****************************************************************************
+ * Author: BalkanBasileus
+ * 
+ * Program Description:
+ *  
+ *
+ *
+ *****************************************************************************/
+using System;
 using System.Collections.Generic;
 
 namespace HashTables {
 
   class Program {
+
     //Options in main menu
     private enum options {
       ONE = 1,
       TWO = 2,
     }
 
+    //MAIN
     static void Main(string[] args) {
 
       //Main Variables
-      int input = 0;
+      int input;
       bool terminate = false;
       char proceed = ' ';
 
@@ -47,13 +57,20 @@ namespace HashTables {
           Program.optionONE(ref hashTable, ref proceed);
           if (proceed == 'y') {
             hashTable.Clear(); //reset
-            continue;
+            continue; //begin again at try statement
           }
           
           break;
 
           //Case TWO  
           case options.TWO:
+          Program.readFile(ref hashTable);
+          Program.optionTWO(ref hashTable, ref proceed);
+
+          if (proceed == 'y') {
+            hashTable.Clear(); //reset
+            continue; //begin again at try statement
+          }
 
           break;
           }
@@ -64,17 +81,12 @@ namespace HashTables {
           continue; // Skip remainder of code and begin again at try statement.
         }
 
-
-
       } while (!terminate);
     }
 
     //METHODS
-    /////////////////////////////////////////////////////////////////////////////
-
-    //Readfile method
-    public void readFile() { }
-
+    /////////////////////////////////////////////////////////////////////////////////
+    
     //Display hash table method
     public static void optionONE(ref SortedDictionary<string, int> hashTable, ref char proceed) {
       /*Pre:
@@ -104,20 +116,22 @@ namespace HashTables {
         }
       }
 
-      //Display Hash Table 
+      //Display Table Header 
       Console.WriteLine();
       Console.WriteLine($"Sorted HashTable: ");
       Console.WriteLine();
 
+      //Header
       Console.WriteLine($" {"Key",-10}{"Value",14}");
 
+      //Display Table
       foreach (var Key in hashTable.Keys) {
 
         Console.WriteLine($"{Key,-10}{hashTable[Key],15}");
 
       }
       Console.WriteLine();
-      Console.WriteLine("Size; " + hashTable.Count);
+      Console.WriteLine("Words: " + hashTable.Count);
 
       //Proceed?
       Console.Write("Continue[y/n]: ");
@@ -130,6 +144,7 @@ namespace HashTables {
         proceed = Console.ReadLine()[0];
       }
 
+      //Write Header for main. Will reprompt menu w/ this on top.
       if (Char.ToLower(proceed) == 'y') {
         Console.WriteLine("///////////////////////////////////////");
         proceed = 'y';
@@ -142,10 +157,88 @@ namespace HashTables {
 
     //optonTWO Method
     public static void optionTWO(ref SortedDictionary<string, int> hashTable, ref char proceed) {
-      /*Pre:
+      /*Pre: Takes hash table and menu driven char by ref.
        *
-       *Post:
+       *Post: Displays contents of hash table read from file by 'readFile' method.
        */
+
+      //Display Table Header 
+      Console.WriteLine();
+      Console.WriteLine($"Sorted HashTable: ");
+      Console.WriteLine();
+
+      //Header
+      Console.WriteLine($" {"Key",-21}{"Value",0}");
+
+      //Display Table
+      foreach (var Key in hashTable.Keys) {
+
+        Console.WriteLine($"{Key,-22}{hashTable[Key],-1}");
+
+      }
+      Console.WriteLine();
+      Console.WriteLine("Words: " + hashTable.Count);
+
+      //Proceed?
+      Console.Write("Continue[y/n]: ");
+      proceed = Console.ReadLine()[0];
+
+      //Error check
+      while (Char.ToLower(proceed) != 'y' && Char.ToLower(proceed) != 'n') {
+
+        Console.Write("Continue[y/n]: ");
+        proceed = Console.ReadLine()[0];
+      }
+
+      //Write Header for main. Will reprompt menu w/ this on top.
+      if (Char.ToLower(proceed) == 'y') {
+        Console.WriteLine("///////////////////////////////////////");
+        proceed = 'y';
+      }
+      else {
+        Console.WriteLine("///////////////////////////////////////");
+        Environment.Exit(0);
+      }
     }
+    
+
+    //Readfile method
+    public static void readFile(ref SortedDictionary<string, int> hashTable) {
+      /*Pre: Take hash table by ref. 
+       *
+       *Post: Reads file by line, splits line, tokenizes words and populates
+       *    hash table. Increments quantity if word repeats
+       */
+
+      //Local Vars
+      string line;
+      string[] stringArray;
+
+      System.IO.StreamReader file = new
+        System.IO.StreamReader(@"/Users/Dimitrov/Projects/HashTables/HashTables/data.txt");
+
+      //Read Line. Tokenize. Map to hash table. Inc value.
+      while( (line = file.ReadLine() ) != null) {
+
+        //tokenize using .split()
+        stringArray = System.Text.RegularExpressions.Regex.Split(line, @" ");
+
+        foreach (var word in stringArray) {
+
+          var key = word.ToLower(); //lower case
+
+          //if hash table contains word
+          if (hashTable.ContainsKey(key)) {
+            hashTable[key]++; //inc value
+          }
+          else { // add new word with a count of 1 to dic.
+            hashTable.Add(key, 1);
+          }
+        }
+
+      }
+      file.Close();
+    }
+
   }
 }
